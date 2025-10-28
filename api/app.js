c4057b0e040fdd721edf45fef1a9ff57f87d0248
handsansanhand <102315833+handsansanhand@users.noreply.github.com>
2025-10-28 15:10:34 +0000

    dockerizations started

--- api/app.js
+++ api/app.js
@@ -1,17 +1,15 @@
 const { PORT } = require("./config");
 const express = require("express");
 const cors = require("cors");
-const basicAPIRoutes = require("./routes/basicAPI");
 const shortestPath = require("./routes/shortestPath");
 const players = require("./routes/players");
 const app = express();
 
-app.use("/", basicAPIRoutes);
 app.use("/shortestPath", shortestPath);
 app.use("/players", players);
 
-app.listen(PORT || 3000, () => {
-  console.log("App is listening on port ", PORT || 3000);
+app.listen(PORT || 3030, () => {
+  console.log("App is listening on port ", PORT || 3030);
 }).on('error', err => {
   console.error("Server failed to start: ", err);
 });
--- api/controllers/basicGet.js
+++
@@ -1,8 +0,0 @@
-const fetch = require("node-fetch");
-
-async function basicGet(req, res) {
-    const correctStr = "You have reached the basic get function successfully.";
-    res.json(correctStr);
-}
-
-module.exports = { basicGet };
