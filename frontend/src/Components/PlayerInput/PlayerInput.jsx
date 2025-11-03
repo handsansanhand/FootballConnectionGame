fa65b687c0a1d5c13bbedc761f46f0f854d37d37
handsansanhand <102315833+handsansanhand@users.noreply.github.com>
2025-11-03 16:40:02 +0000

    guess bars finally work

--- frontend/src/Components/PlayerInput/PlayerInput.jsx
+++ frontend/src/Components/PlayerInput/PlayerInput.jsx
@@ -1,23 +1,40 @@
 import SearchBar from "../SearchBar/SearchBar";
+import SearchBarGuess from "../SearchBar/SearchBarGuess";
 
-function PlayerInput({ label, playerKey, setPlayer, handleReset, hasRandomChoice, wrongGuessTrigger }) {
-
+function PlayerInput({
+  label,
+  playerKey,
+  setPlayer,
+  handleReset,
+  hasRandomChoice,
+  wrongGuessTrigger,
+  correctGuessTrigger,
+  hasGuess, // new prop
+}) {
   return (
-    <>
-      <div className="flex-1">
-        <div className="bg-gray-50 dark:bg-gray-700 rounded-lg p-4 border border-gray-300 dark:border-gray-600 shadow">
-          <h2 className="text-xl font-bold mb-2 text-gray-700 dark:text-gray-200">
-            { label }
-          </h2>
+    <div className="flex-1">
+      <div className="bg-gray-50 dark:bg-gray-700 rounded-lg p-4 border border-gray-300 dark:border-gray-600 shadow">
+        <h2 className="text-xl font-bold mb-2 text-gray-700 dark:text-gray-200">
+          {label}
+        </h2>
+
+        {hasGuess ? (
+          <SearchBarGuess
+            onSubmit={(selected) => setPlayer(selected)}
+            onReset={() => handleReset(playerKey)}
+            wrongGuessTrigger={wrongGuessTrigger}
+            correctGuessTrigger={correctGuessTrigger}
+          />
+        ) : (
           <SearchBar
             onSubmit={(selected) => setPlayer(selected)}
             onReset={() => handleReset(playerKey)}
             hasRandomChoice={hasRandomChoice}
             wrongGuessTrigger={wrongGuessTrigger}
           />
-        </div>
+        )}
       </div>
-    </>
+    </div>
   );
 }
 
