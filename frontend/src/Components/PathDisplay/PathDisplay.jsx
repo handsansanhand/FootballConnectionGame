8fe30f0212f36409286692cea3bd89e7bcd3e185
handsansanhand <102315833+handsansanhand@users.noreply.github.com>
2025-11-25 13:49:27 +0000

    started designing the shortest path + edges

--- frontend/src/Components/PathDisplay/PathDisplay.jsx
+++ frontend/src/Components/PathDisplay/PathDisplay.jsx
@@ -2,15 +2,26 @@ import SearchBar from "../../Components/SearchBar/SearchBar";
 import { useNavigate } from "react-router-dom";
 import Graph from "../Graph/Graph";
 import { useEffect } from "react";
-function PathDisplay( { player1, player2, path, errorMessage, isMobile } ) {
-
+function PathDisplay({ player1, player2, path, errorMessage, isMobile }) {
   return (
     <>
-      <div className="relative bg-gray-100 rounded border-4 border-red-500 w-full min-h-[550px] h-[75vh] flex items-center justify-center text-center">
+      <div
+        className="relative rounded w-full min-h-[550px] h-[75vh] flex items-center justify-center text-center"
+        style={{
+          backgroundColor: "rgba(18, 195, 1, 0.65)",
+          backgroundImage:
+            "repeating-linear-gradient(90deg, rgba(255,255,255,1) 1px, rgba(255,255,255,1) 2px, transparent 4px, transparent 150px)"
+        }}
+      >
         {errorMessage ? (
           <p className="text-red-600 font-semibold">{errorMessage}</p>
         ) : path ? (
-          <Graph pathJson={path} playerA={player1} playerB={player2} isMobile={isMobile}/>
+          <Graph
+            pathJson={path}
+            playerA={player1}
+            playerB={player2}
+            isMobile={isMobile}
+          />
         ) : (
           <p className="text-gray-500">
             No path calculated yet. Enter players below.
